f46b350a9962d0156a6bcd85ee5061507acb9748
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-19 23:41:05 +0000

    webpack config changes and build file

--- widget-onboarding/src/App.js
+++ widget-onboarding/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import WidgetOnboarding from './components/WidgetOnboarding';
+import React from 'react';
 
 function App() {
   return (
--- widget-onboarding/src/components/WidgetOnboarding.js
+++ widget-onboarding/src/components/WidgetOnboarding.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import React from 'react';
 
 const PageWrapper = styled.div`
   display: flex;

9c29b0752870fa32898dd4c55045e6c0acb64753
Madhav Manohar S <madhavmanohar.s@gmail.com>
2023-03-19 22:29:07 +0000

    first commit

---
+++ widget-onboarding/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WidgetOnboarding from './components/WidgetOnboarding';
+
+function App() {
+  return (
+    <div className="App">
+      <WidgetOnboarding/>
+    </div>
+  );
+}
+
+export default App;
